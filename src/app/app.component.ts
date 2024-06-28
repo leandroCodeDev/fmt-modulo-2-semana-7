386e517306b2427026824a3fe514ce5ea65bd388
Leandro Dias <lvd@certi.org.br>
2024-06-28 01:24:57 -0300

    iniciado as atividades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterModule} from '@angular/router';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { SidebarComponent } from '../shared/components/sidebar/sidebar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterModule,
+    HeaderComponent,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/cadastro-aluno/cadastro-aluno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-aluno',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-aluno.component.html',
+  styleUrl: './cadastro-aluno.component.scss'
+})
+export class CadastroAlunoComponent {
+
+}
